115810c4354d36e0831dc447d3758fd224e1020e
HatimGW <138097722+HatimGW@users.noreply.github.com>
2024-01-07 13:04:19 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -18,16 +18,11 @@ const{userID}=useSelector(state=>state.items8)
         const response = await axios.get(`${BASE_URL}/check`,{withCredentials:true,credentials:"include"})
         
         if(response.data.success){
+        socket.emit('login', response.data.userID);
         socket.emit('updateCart', response.data.userID);
-        socket.on('cartUpdated', (data) => {
-          console.log(data)
-          dispatch(cartUpdated(data));
-      });
         dispatch(users(response.data.username))
         dispatch(loginSuccess(response.data.userID))
         dispatch(message2(response.data.success))
-        
-
         }
         else{
             dispatch(users(""))
@@ -40,16 +35,10 @@ const{userID}=useSelector(state=>state.items8)
 }
 
   useEffect(() => {
-    console.log('Attempting to set up socket listener for cartUpdated');
     socket.on('cartUpdated', (data) => {
-      console.log(data)
       dispatch(cartUpdated(data));
   });
-  
-    return () => {
-      console.log('Cleaning up socket listener for cartUpdated');
-      socket.disconnect()
-    };
+
   },[userID,dispatch]);
   
   useEffect(()=>{
