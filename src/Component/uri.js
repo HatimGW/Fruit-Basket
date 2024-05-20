bea1be60df716b59b9d2aabd58c9df575a107287
Hatim Ghoriwala <hatimghoriwala00881@gmail.com>
2024-05-20 13:10:01 +0530

    Update uri.js

--- src/Component/uri.js
+++ src/Component/uri.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://fruitbackend-32zf.onrender.com"
+export const BASE_URL = "https://fruitbackend-tpgb.onrender.com"
 // export const BASE_URL = "http://localhost:3001"
