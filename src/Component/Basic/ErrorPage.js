7b68bf45240ce6e8c095af06e08fa110b7a0b4ef
Hatim Ghoriwala <hatimghoriwala00881@gmail.com>
2024-07-18 21:05:29 +0530

    Update ErrorPage.js

--- src/Component/Basic/ErrorPage.js
+++ src/Component/Basic/ErrorPage.js
@@ -3,8 +3,8 @@ import React from "react"
 const ErrorPage = () => {
   return (
     <section>
-    <h1>404 - Page Not Found </h1>
-    <p>Sorry, the page you are looking for does not exist.</p>
+    <h1 style={{fontFamily:"serif", fontSize:"3em"}}>404 - Page Not Found </h1>
+    <p style={{fontFamily:"serif", fontSize:"2em"}}>Sorry, the page you are looking for does not exist.</p>
     </section>
     )
 }
